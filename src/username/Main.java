7d71e45439d4af01810508859d798c356ff8e8f2
Charli Murphy <n11245891@qut.edu.au>
2024-04-22 13:38:20 +1000

    - added more authen

--- src/username/Main.java
+++ src/username/Main.java
@@ -1,10 +1,14 @@
 package username;
-
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.List;
 public class Main {
     public static void main(String[] args) {
 
         // TODO: Implement the main method
-        User username = new Username();
-        username.setUsername();
+        User username = new User("van@example", "pass", "Vannessa", "Hill", 0);
     }
+        
 }
--- src/username/User.java
+++ src/username/User.java
@@ -1,20 +1,55 @@
 package username;
 
+import java.sql.Connection;
+import java.util.List;
+
 public class User {
 
     private int id;
     private String username;
     private String password;
-    private String firstname;
-    private String lastname;
+    private String firstName;
+    private String lastName;
     private int age;
 
 
-    public User(String username, String password, String firstname, String lastname, int age) {
+    public User(String username, String password, String firstName, String lastName, int age) {
         this.username = username;
         this.password = password;
-        this.firstname = firstname;
-        this.lastname = lastname;
+        this.firstName = firstName;
+        this.lastName = lastName;
         this.age = age;
     }
+    
+    public int getId() {
+        return id;
+    }
+    public int getage() {
+        return age;
+    }
+    public String getusername() {
+        return username;
+    }
+
+    public String getpassword() {
+        return password;
+    }
+
+    public String getfirstName() {
+        return firstName;
+    }
+
+    public String getlastName() {
+        return lastName;
+    }
+    public void setName(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public void setUserandPass(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
 }
