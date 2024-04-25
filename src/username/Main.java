691ff1f2a9393693cb4b1d06e2197ab818bc0fe0
Charli Murphy <n11245891@qut.edu.au>
2024-04-25 15:40:05 +1000

    - Added database beginnings

--- src/username/Main.java
+++ src/username/Main.java
@@ -1,14 +1,15 @@
 package username;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.sql.Statement;
-import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
+        // Establish a connection to the database
+            UserDAO userDAO = new UserDAO();
+            userDAO.createTable();
+            // Create a new user
+            userDAO.insert(new User("van@example", "pass", "Vannessa", "Hill", 24));
 
-        // TODO: Implement the main method
-        User username = new User("van@example", "pass", "Vannessa", "Hill", 0);
+            // Update the user's name and age
+            // Insert the new user into the database
+            userDAO.close();
     }
-        
 }
--- src/username/User.java
+++ src/username/User.java
@@ -1,7 +1,8 @@
 package username;
 
 import java.sql.Connection;
-import java.util.List;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
 public class User {
 
@@ -11,8 +12,14 @@ public class User {
     private String firstName;
     private String lastName;
     private int age;
-
-
+    public User(int id, String username, String password, String firstName, String lastName, int age) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+    }
     public User(String username, String password, String firstName, String lastName, int age) {
         this.username = username;
         this.password = password;
@@ -20,36 +27,53 @@ public class User {
         this.lastName = lastName;
         this.age = age;
     }
-    
+
     public int getId() {
         return id;
     }
-    public int getage() {
+
+    public int getAge() {
         return age;
     }
-    public String getusername() {
+
+    public String getUsername() {
         return username;
     }
 
-    public String getpassword() {
+    public String getPassword() {
         return password;
     }
 
-    public String getfirstName() {
+    public String getFirstName() {
         return firstName;
     }
 
-    public String getlastName() {
+    public String getLastName() {
         return lastName;
     }
+
     public void setName(String firstName, String lastName) {
         this.firstName = firstName;
         this.lastName = lastName;
     }
 
-    public void setUserandPass(String username, String password) {
+    public void setUsernameAndPassword(String username, String password) {
         this.username = username;
         this.password = password;
     }
+    
 
+    public void insertData(Connection connection) {
+        String insertQuery = "INSERT INTO users (username, password, first_name, last_name, age) VALUES (?, ?, ?, ?, ?)";
+        try (PreparedStatement statement = connection.prepareStatement(insertQuery)) {
+            statement.setString(1, getUsername());
+            statement.setString(2, getPassword());
+            statement.setString(3, getFirstName());
+            statement.setString(4, getLastName());
+            statement.setInt(5, getAge());
+            statement.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
 }
